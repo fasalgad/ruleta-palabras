0c535913c99b1ae73b8621ef2faef528f51f7d0c
Gustavox25 <gseguraurrutia@gmail.com>
2020-07-08 14:47:32 -0400

    Se actualizo

---
+++ src/views/Grid.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" sm="4">
+        <p>La ruleta palabra consiste en bla bla bla</p>
+      </v-col>
+      <v-col cols="12" sm="8">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+          cupiditate totam optio ipsa fugiat. Accusamus dolorum, magni, eum
+          eligendi consequuntur quo, tempora ipsa dolore possimus voluptatum
+          repellendus. Accusantium, explicabo dolorum?
+        </p>
+        <v-layout>
+          <v-flex>
+            <v-btn dark color="green">
+              Comenzar Juego
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
